efd83b26b9903950ee07b55aca1c4115f0dc1427
Eudita <adiyadav1592@gmail.com>
2025-06-21 07:57:40 +0530

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,9 @@
 import os
 import sys
+
 import numpy as np 
 import pandas as pd
-import dill 
+import dill
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
@@ -15,26 +16,27 @@ def save_object(file_path, obj):
 
         os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path, 'wb') as file_obj:
+        with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
-
-
-def evaluate_models(X_train, y_train,X_test,y_test,models, params, ):
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            para = params[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            gs = GridSearchCV(model, para, cv=3)
-            gs.fit(X_train, y_train) 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(X_train, y_train)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
@@ -51,10 +53,10 @@ def evaluate_models(X_train, y_train,X_test,y_test,models, params, ):
     except Exception as e:
         raise CustomException(e, sys)
     
-
 def load_object(file_path):
     try:
-        with open(file_path, 'rb') as file_obj:
-            return dill.load(file_obj)
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
